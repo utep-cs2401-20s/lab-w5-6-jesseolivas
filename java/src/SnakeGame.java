7b2d96c36c774c710c330f4e61e71e0dcebf9e41
jesseolivas <jesseolivas@live.com>
2020-02-28 01:07:28 -0700

    Commented SnakeGame.java

--- java/src/SnakeGame.java
+++ java/src/SnakeGame.java
@@ -5,11 +5,12 @@ public class SnakeGame {
     private static int exhaustiveChecks;
     private static int recursiveChecks;
 
-
+    // Default constructor that makes board of size [1][1]
     public SnakeGame(){
         game = new boolean[1][1];
     }
 
+    // Constructor that takes in board, and head position.
     public SnakeGame(boolean[][] g, int x, int y){
         int size = g.length;
         headPosition = new int[2];
@@ -23,14 +24,14 @@ public class SnakeGame {
         headPosition [1] = y;
     }
 
-
+    // Method to find the tail using exhaustive means, i.e. loops
     public int[] findTailExhaustive(){
         resetCounter();
         int[] t = new int[3];
         int length = 0;
+
         // Start at the first cell
         // check if part of snake, if not, check neighbors, then check next cell
-        //
         for(int i = 0; i < game.length; i++){
             for(int j = 0; j < game[i].length;j++){
                 exhaustiveChecks ++;
@@ -48,6 +49,7 @@ public class SnakeGame {
         return t;
     }
 
+    // Supplementary method to find how many live cells around the current cell
     public int neighbors(int row, int col){
         // Counter to add and return each cell's neighbors
         int count = 0;
@@ -73,17 +75,21 @@ public class SnakeGame {
         return count;
     }
 
+    // Recursive method that calls a private method that does all the actual work
     public int[] findTailRecursive(){
         resetCounter();
         int[] t = findTailRecursive(headPosition,headPosition);
         return new int[]{t[0],t[1],getLength()};
     }
 
+    // **Diana Valles Helped me get this method to work properly, all other parts of my code were created independently**
+    // Takes the headPosition to avoid mixing up tail and head and finds the tail recursively
     private int[] findTailRecursive(int[] currentPosition, int[] previousPosition){
         int[] nextPosition = new int[2];
         nextPosition[0] = currentPosition[0];
         nextPosition[1] = currentPosition[1];
-        // Base case
+
+        // Base case, returned once tail is found
         if((nextPosition[0] != headPosition[0] && nextPosition[1] == headPosition[1]) || (nextPosition[0] == headPosition[0] && nextPosition[1] != headPosition[1]) || (nextPosition[0] != headPosition[0] && nextPosition[1] != headPosition[1]) &&neighbors(nextPosition[0],nextPosition[1]) == 1){
             if(neighbors(nextPosition[0],nextPosition[1]) == 1) {
                 System.out.println("testb");
@@ -124,9 +130,11 @@ public class SnakeGame {
             return findTailRecursive(nextPosition, currentPosition);
         }
 
+        // Default return statement, shouldn't ever reach here
         return new int[2];
     }
 
+    // Supplementary method for findTailRecursive to find the length
     private int getLength(){
         int l = 0;
         for(int i = 0; i < game.length; i++){
@@ -138,19 +146,21 @@ public class SnakeGame {
         }
         return l;
     }
+
+    // Resets each checks' counter, used at the start of each method
     private void resetCounter(){
         exhaustiveChecks = 0;
         recursiveChecks = 0;
     }
 
+    // Getter for exhaustive checks
     public static int getExhaustiveChecks(){
         return exhaustiveChecks;
     }
 
+    // Getter for recursive checks
     public static int getRecursiveChecks(){
         return recursiveChecks;
     }
-    public void printBoard(){
 
-    }
 }
